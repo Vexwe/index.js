d48a88884bacd7fb1acbc98990d442fe792ce88d
Vexwe <83681357+Vexwe@users.noreply.github.com>
2021-05-21 11:09:30 -0300

    Coloquei mais portas
    Basicamente eu fui testando essas novas portas apenas para fixar na cabeça o "app.get" e o "res.send"

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");  //importando o express
 const app = express(); //  Iniciando o express e  passando pra var app
 
+
+app.get("/",function(req,res){
+ res.send("Bem vindo ao guia do programador iniciante");
+});
+app.get("/rickroll",function(req,res){
+res.send("<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>Iphone gratis aq</a>")
+});
+
+app.get("/a", function(req,res){
+   res.send( "<h1>aaaae</h1>")
+   
+})
 app.listen(4000,function(erro){
     if(erro){
         console.log("ocorreu um erro");
